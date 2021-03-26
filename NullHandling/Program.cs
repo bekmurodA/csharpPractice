3d7ad2481bce158356e72c32f96aa385c4af681b
Bekmurod <7248774american@gmail.com>
2021-03-26 22:02:43 +0500

    C sharp first app

---
+++ NullHandling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NullHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	int thisCannotBeNull = 4;
+	//cnt cnv int to null	thisCannotBeNull = null; //compile error
+
+		int? thisCouldBeNull = null;
+		Console.WriteLine(thisCouldBeNull);
+		Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+		thisCouldBeNull = 7;
+		
+		Console.WriteLine(thisCouldBeNull);
+		Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+	}
+    }
+}
