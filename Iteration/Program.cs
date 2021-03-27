febae6847d0e00da28d32320691e21641ce3932a
bekmurodA <7248774american@gmail.com>
2021-03-27 15:09:04 +0500

    Switch

---
+++ Iteration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		/*int x = 0;
+		while (x<3)
+		{
+			WriteLine(x);
+			x++;
+		}
+		string password = string.Empty;
+		do 
+		{
+			Write("Enter your password:  ");
+			password = ReadLine();
+
+		}
+		while (password != "pass");
+		
+		WriteLine("Correct!");*/
+
+		string[] names = {"Adam","Barry","Charlie"};
+		foreach(string name in names)
+		{
+			WriteLine($"{name} has {name.Length} characters");
+		}
+        }
+    }
+}
---
+++ Switch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string path = @"./";
+	   Write("Press R for readonly or W for write: ");
+	   ConsoleKeyInfo key = ReadKey();
+	   WriteLine();
+
+	   Stream s = null;
+	   if (key.Key == ConsoleKey.R)
+	   {
+		s = File.Open(
+			Path.Combine(path,"file.txt"),
+			FileMode.OpenOrCreate,
+			FileAccess.Read);
+	   }
+	   else 
+	   {
+		s = File.Open(
+			Path.Combine(path,"file.txt"),
+			FileMode.OpenOrCreate,
+			FileAccess.Write);
+	   }
+	   string message = string.Empty;
+	   switch(s)
+	   {
+		case FileStream writetableFile when s.CanWrite:
+			message = "The stream is a file that I can write to";
+			break;
+		case FileStream readOnlyFile:
+			message = "The stream is read-only file";
+			break;
+		case MemoryStream ms:
+			message = "The stream is a memory address";
+			break;
+		default: //always evaluated last
+			message = "The stream is some other type.";
+			break;
+		case null:
+			message = "The stream is null";
+			break;
+	   }
+	   WriteLine(message);
+        }
+    }
+}
