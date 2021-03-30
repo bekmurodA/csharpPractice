29b64fbfef362d48b1113eaddabc86cd181d09ef
bekmurodA <7248774american@gmail.com>
2021-03-30 17:43:58 +0500

    hashing algos from ch10

---
+++ chapter10Protection/EncryptionApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+using Packt.Shared;
+using System.Security.Cryptography; // CryptographicException
+
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	Write("Enter a message that you want to encrypt: ");
+		string message = ReadLine();
+
+		Write("Enter a password: ");
+		string password = ReadLine();
+
+		string cryptoText = Protector.Encrypt(message,password);
+		WriteLine($"Encrypted text: {cryptoText}");
+
+		Write("Enter the password: ");
+		string password2 = ReadLine();
+
+		try
+		{
+		  string clearText = Protector.Decrypt(cryptoText,password2);
+		  WriteLine($"Decrypted text: {clearText}");
+		}
+		catch (CryptographicException ex)
+		{
+			WriteLine("{0}\n More details: {1}",
+			"You entered a wrong password",ex.Message);
+		}
+		catch (Exception ex)
+		{
+			WriteLine("Non Cryptographic exception: {0}, {1}",
+			arg0:ex.GetType().Name,
+			arg1:ex.Message);
+		}
+	}
+
+    }
+}
---
+++ chapter10Protection/hashingApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+using System;
+using Packt.Shared;
+
+namespace hashingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	WriteLine("Registering Alice with Pa$$w0rd.");
+		var alice = Protector.Register("Alice","Pa$$w0rd");
+
+		WriteLine($"Name: {alice.Name}");
+		WriteLine($"Salt: {alice.Salt}");
+		WriteLine("Password (salted and hashed): {0}",alice.SaltedHashedPassword);
+		WriteLine();
+
+	}
+
+    }
+}
