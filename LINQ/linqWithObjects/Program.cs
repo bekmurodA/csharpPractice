63fc700c762d2439f33a165ead636d3c8d3a21fe
bekmurodA <7248774american@gmail.com>
2021-03-31 14:35:47 +0500

    Linq comprehension syntax

--- LINQ/linqWithObjects/Program.cs
+++ LINQ/linqWithObjects/Program.cs
@@ -6,6 +6,28 @@ namespace linqWithObjects
 {
     class Program
     {
+	static void LinqWithArrayOfExceptions()
+	{
+	  var errors = new Exception[]
+	  {
+		new ArgumentException(),
+		new SystemException(),
+		new IndexOutOfRangeException(),
+		new InvalidOperationException(),
+		new NullReferenceException(),
+		new InvalidCastException(),
+		new OverflowException(),
+		new DivideByZeroException(),
+		new ApplicationException(),
+	  };
+
+	  var numberErrors = errors.OfType<ArithmeticException>();
+
+	  foreach (var e in numberErrors)
+	  {
+		WriteLine(e);
+	  }
+	}
         static void Main(string[] args)
         {
         	static void linqWithArrayOfStrings()
@@ -13,21 +35,27 @@ namespace linqWithObjects
 			var names = new string[] {"Michael","Pam","Jim","Dwight","Angela",
 			"Kevin","Toby","Creed"};
 
-			var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+			//var query = names.Where(new Func<string, bool>(NameLongerThanFour));
 			//Func<string,bool> input:string output:bool
 
+			//var query = names.Where(NameLongerThanFour);
+			var query = names
+				.Where(name=>name.Length>4)
+				.OrderBy(name=>name.Length)
+				.ThenBy(name=>name);
 			foreach(string item in query)
 			{
 				WriteLine(item);
 			}
 		}
-		linqWithArrayOfStrings();
+		//linqWithArrayOfStrings();
+		LinqWithArrayOfExceptions();
 	}
 
-	static bool NameLongerThanFour(string name)
+	/*static bool NameLongerThanFour(string name)
 	{
 		return name.Length > 4; 
-	}
+	}*/
 
     }
 }
